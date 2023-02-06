d64dea493246d7608bde9504e9a901dfe8e9b1b5
Christian De Angelis <christiandeange@gmail.com>
2023-02-06 01:03:10 -0500

    Add Usage attribute to aar-publishing configuration.

--- gradle-plugin/src/main/kotlin/sh/christian/aaraar/gradle/AarAarPlugin.kt
+++ gradle-plugin/src/main/kotlin/sh/christian/aaraar/gradle/AarAarPlugin.kt
@@ -11,6 +11,7 @@ import com.android.build.api.variant.Variant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE
+import org.gradle.api.attributes.Usage.USAGE_ATTRIBUTE
 import org.gradle.api.component.SoftwareComponentFactory
 import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.getByType
@@ -84,6 +85,10 @@ class AarAarPlugin
       isTransitive = true
       isCanBeConsumed = true
       isCanBeResolved = true
+
+      attributes {
+        attribute(USAGE_ATTRIBUTE, objects.named("embed-aar"))
+      }
     }
 
     val aaraar = extensions.getByType<AarAarExtension>()
