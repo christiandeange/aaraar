d665da5c9eee61f6c233f0405df52f5ff9e40e20
Christian De Angelis <christiandeange@gmail.com>
2024-02-01 02:58:35 -0500

    Bump JDK version to 17.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,6 @@ tasks.withType<DokkaCollectorTask>().configureEach {
 
 java {
   toolchain {
-    languageVersion = JavaLanguageVersion.of(11)
+    languageVersion = JavaLanguageVersion.of(17)
   }
 }
