fd05d9745e26048aa64442d05766faa096d97931
Christian De Angelis <christiandeange@gmail.com>
2024-03-04 18:46:29 -0500

    Sort Modifiers according to preferred ordering.

--- core/src/main/kotlin/sh/christian/aaraar/model/classeditor/Modifier.kt
+++ core/src/main/kotlin/sh/christian/aaraar/model/classeditor/Modifier.kt
@@ -7,39 +7,43 @@ import javassist.Modifier as JModifier
  */
 enum class Modifier {
   PUBLIC,
-  PRIVATE,
   PROTECTED,
+  PRIVATE,
+  ABSTRACT,
   STATIC,
   FINAL,
-  SYNCHRONIZED,
-  VOLATILE,
-  VARARGS,
   TRANSIENT,
+  VOLATILE,
+  SYNCHRONIZED,
   NATIVE,
-  INTERFACE,
-  ABSTRACT,
-  STRICT,
+  STRICTFP,
+  VARARG,
   ANNOTATION,
+  INTERFACE,
   ENUM,
   ;
+
+  override fun toString(): String {
+    return name.lowercase()
+  }
 
   internal companion object {
     fun fromModifiers(modifiers: Int): Set<Modifier> {
       return setOfNotNull(
         PUBLIC.takeIf { JModifier.isPublic(modifiers) },
-        PRIVATE.takeIf { JModifier.isPrivate(modifiers) },
         PROTECTED.takeIf { JModifier.isProtected(modifiers) },
+        PRIVATE.takeIf { JModifier.isPrivate(modifiers) },
+        ABSTRACT.takeIf { JModifier.isAbstract(modifiers) },
         STATIC.takeIf { JModifier.isStatic(modifiers) },
         FINAL.takeIf { JModifier.isFinal(modifiers) },
-        SYNCHRONIZED.takeIf { JModifier.isSynchronized(modifiers) },
-        VOLATILE.takeIf { JModifier.isVolatile(modifiers) },
-        VARARGS.takeIf { JModifier.isVarArgs(modifiers) },
         TRANSIENT.takeIf { JModifier.isTransient(modifiers) },
+        VOLATILE.takeIf { JModifier.isVolatile(modifiers) },
+        SYNCHRONIZED.takeIf { JModifier.isSynchronized(modifiers) },
         NATIVE.takeIf { JModifier.isNative(modifiers) },
-        INTERFACE.takeIf { JModifier.isInterface(modifiers) },
-        ABSTRACT.takeIf { JModifier.isAbstract(modifiers) },
-        STRICT.takeIf { JModifier.isStrict(modifiers) },
+        STRICTFP.takeIf { JModifier.isStrict(modifiers) },
+        VARARG.takeIf { JModifier.isVarArgs(modifiers) },
         ANNOTATION.takeIf { JModifier.isAnnotation(modifiers) },
+        INTERFACE.takeIf { JModifier.isInterface(modifiers) },
         ENUM.takeIf { JModifier.isEnum(modifiers) },
       )
     }
@@ -48,19 +52,19 @@ enum class Modifier {
       return map {
         when (it) {
           PUBLIC -> JModifier.PUBLIC
-          PRIVATE -> JModifier.PRIVATE
           PROTECTED -> JModifier.PROTECTED
+          PRIVATE -> JModifier.PRIVATE
+          ABSTRACT -> JModifier.ABSTRACT
           STATIC -> JModifier.STATIC
           FINAL -> JModifier.FINAL
-          SYNCHRONIZED -> JModifier.SYNCHRONIZED
-          VOLATILE -> JModifier.VOLATILE
-          VARARGS -> JModifier.VARARGS
           TRANSIENT -> JModifier.TRANSIENT
+          VOLATILE -> JModifier.VOLATILE
+          SYNCHRONIZED -> JModifier.SYNCHRONIZED
           NATIVE -> JModifier.NATIVE
-          INTERFACE -> JModifier.INTERFACE
-          ABSTRACT -> JModifier.ABSTRACT
-          STRICT -> JModifier.STRICT
+          STRICTFP -> JModifier.STRICT
+          VARARG -> JModifier.VARARGS
           ANNOTATION -> JModifier.ANNOTATION
+          INTERFACE -> JModifier.INTERFACE
           ENUM -> JModifier.ENUM
         }
       }.fold(0, Int::or)
